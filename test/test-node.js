1823402e25e3729194ba58ed0eeba87f644bd5a1
xuchenhui <xu.chenhui@live.com>
2014-12-17 13:43:08 +0800

    Update package.json

--- test/test-node.js
+++ test/test-node.js
@@ -1,5 +1,5 @@
-var BackboneObserve = require("./src/backbone-observe.js");
-
+//var BackboneObserve = require("./src/backbone-observe.js");
+var BackboneObserve = require("backbone.observe");
 var om = new BackboneObserve.model({id: 'id', foo: 1, bar: 2, baz: 3});
 om.on("change:foo", function(model, val) {
     if (val == 2){
